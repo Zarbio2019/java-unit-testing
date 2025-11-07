a1bb26917da55d7c6a29e6cd8658bf069786b179
Romulo <zarod2019@gmail.com>
2025-11-07 11:53:39 -0500

    Add SpringBoot projects

---
+++ springboot/spring-boot-testing-reddit-clone/target/generated-sources/annotations/com/programming/techie/springredditclone/mapper/SubredditMapperImpl.java
@@ -0,0 +1,49 @@
+package com.programming.techie.springredditclone.mapper;
+
+import com.programming.techie.springredditclone.dto.SubredditDto;
+import com.programming.techie.springredditclone.dto.SubredditDto.SubredditDtoBuilder;
+import com.programming.techie.springredditclone.model.Subreddit;
+import com.programming.techie.springredditclone.model.Subreddit.SubredditBuilder;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-01-16T21:49:24-0500",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_281 (Oracle Corporation)"
+)
+@Component
+public class SubredditMapperImpl implements SubredditMapper {
+
+    @Override
+    public SubredditDto mapSubredditToDto(Subreddit subreddit) {
+        if ( subreddit == null ) {
+            return null;
+        }
+
+        SubredditDtoBuilder subredditDto = SubredditDto.builder();
+
+        subredditDto.id( subreddit.getId() );
+        subredditDto.name( subreddit.getName() );
+        subredditDto.description( subreddit.getDescription() );
+
+        subredditDto.numberOfPosts( mapPosts(subreddit.getPosts()) );
+
+        return subredditDto.build();
+    }
+
+    @Override
+    public Subreddit mapDtoToSubreddit(SubredditDto subredditDto) {
+        if ( subredditDto == null ) {
+            return null;
+        }
+
+        SubredditBuilder subreddit = Subreddit.builder();
+
+        subreddit.id( subredditDto.getId() );
+        subreddit.name( subredditDto.getName() );
+        subreddit.description( subredditDto.getDescription() );
+
+        return subreddit.build();
+    }
+}
